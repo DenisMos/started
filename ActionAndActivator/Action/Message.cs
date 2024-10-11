30bd11a4ddc50bc532ac2c2ee4d8237de608dbfd
denis <denis@DESKTOP-I5LBTCE>
2024-10-11 18:33:55 +0300

    example

---
+++ ActionAndActivator/Action/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.started.ActionAndActivator.Action
+{
+	internal class Message : ActionBase
+	{
+		[SerializeField]
+		private string _message;
+
+		public override void Execute(ActivatorBase activator)
+		{
+			Debug.Log($"{_message}");
+		}
+	}
+}
